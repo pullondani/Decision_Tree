d45fb951d795267a8422b9d73c17d136efd790b8
Daniel Pullon <danielpullon@gmail.com>
2021-06-22 23:01:33 +1200

    Adds data and output

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,16 @@ import java.io.IOException;
 public class Main {
 
     /**
-     * @param args 2 arguments for hepatitis training and test,
-     *             1 argument for the DIRECTORY of where the CROSS VALIDATION files are
+     * Program to build a decision tree based on data supplied from Victoria University of Wellington.
+     * @param args boolean - true, program runs with Cross Validation
+     *
      */
     public static void main(String[] args) {
         try {
-            if (args.length > 1) {
-                new DecisionTree(args[0], args[1]);
+            if (args.length > 0 && args[0].equals("true")) {
+                new DecisionTree("./data");
             } else {
-                new DecisionTree(args[0]);
+                new DecisionTree("./data/hepatitis-training", "./data/hepatitis-test");
             }
         } catch (IOException e) {
             throw new RuntimeException("The file writer threw an exception");
